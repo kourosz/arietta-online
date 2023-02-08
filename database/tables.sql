c049fefd2f5478d95285d532843721fbb0f0d6c9
Kouros <kourosz@gmail.com>
2023-02-08 23:37:41 +1100

    add sql tables

--- database/tables.sql
+++ database/tables.sql
@@ -22,12 +22,13 @@ CREATE TYPE Attribute as ENUM (
 
 DROP TABLE Product;
 create table if not exists Product (
-    id UUID Primary Key,
+    id UUID Primary Key default gen_random_uuid(),
     name varchar(100) NOT NULL,
     productType ProductType NOT NULL,
     images JSONB,
     company Companies NOT NULL,
     description VARCHAR(1024),
+    extra JSONB null,
     UNIQUE(name)
 );
 
@@ -38,8 +39,8 @@ CREATE TABLE AttributeValue (
 );
 
 CREATE TABLE ProductVariant (
-    id UUID Primary Key,
-    productId UUID NOT NULL,
+    id UUID Primary Key default gen_random_uuid(),
+    productId UUID NOT NULL References Product(id),
     attributeId1 int not null References AttributeValue(id),
     attributeId2 int References AttributeValue(id),
     attributeId3 int References AttributeValue(id),
@@ -48,14 +49,66 @@ CREATE TABLE ProductVariant (
     unique(productId,attributeId1,attributeId2,attributeId3,attributeId4,attributeId5)
 );
 
+Create TABLE Inventory (
+    id Primary Key,
+    productVariantId uuid NOT NULL References ProductVariant(id),
+    date datetime NOT NULL,
+    qty int2 not null
+);
+
+/* 
 INSERT INTO Product 
     (id, name, productType, images, company, price, definedVariants, variants, description) 
     VALUES 
     (gen_random_uuid(), 'MasterSpin Special', 'Rubber', '["masterSpinSpecial.jpeg"]', 'JUIC', 45.00, '["Color","Thickness"]', '[ { "combination": ["Red", "1.5"], "qty": 1} ]', 'Medium pimpled rubber with strenghten pimple-base for increased durability in offense play. Rather thin, relatively much spaced out pimples for increased disturbing effect.');
 
+*/
 
 DROP TABLE Inventory;
 CREATE TABLE Inventory (
-    productId UUID,
-    variant 
-)
+    productVariantId UUID Primary Key,
+    qty int,
+    lastUpdated timestamp default current_timestamp()
+);
+
+CREATE TABLE UserAccount (
+    id UUID Primary Key,
+    fullName varchar(100) not null,
+    address varchar(250),
+    postalAddress varchar(250) null,
+    country varchar(20) not null default('au'),
+    password varchar(256),
+    createdTime timestamp not null default current_timestamp(),
+    updatedTime timestamp not null default current_timestamp(),
+    UNIQUE(fullName)
+);
+
+CREATE TYPE TransactionType as ENUM ('IN', 'OUT');
+
+CREATE TYPE Currency as ENUM ('AUD', 'EURO');
+
+CREATE SEQUENCE invoice_number;
+
+CREATE TABLE Transaction (
+    id int Primary Key,
+    transactionType TransactionType not null,
+    serialNo INT DEFAULT nextval('invoice_number'),
+    date date not null,
+    company Companies null,
+    userAccountId UUID null,
+    currency Currency not null,
+    shipping decimal(4,2) default 0,
+    exchangeRate decimal,
+    unique (serialNo)
+);
+
+drop table TransactionItems;
+CREATE table TransactionItems (
+    transactionId int References Transaction(id),
+    productVariantId UUID not null References ProductVariant(id),
+    qty int not null,
+    rownum INT not null,
+    price decimal(4,2),
+    UNIQUE(transactionId, rownum)
+
+);
